dd436ad6e9c582a48b1c6d52e2d58b27814a5495
Harsh <hs6682100@gmail.com>
2023-12-20 19:55:50 +0000

    more conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -11,13 +11,35 @@
 // true => 1;
 // false => 0
 
-let isLoggedIn= "Harsh"
+// let isLoggedIn= "Harsh"
 
-let booleanisLoggedIn= Boolean(isLoggedIn)
-console.log(typeof booleanisLoggedIn)
-console.log(booleanisLoggedIn)
+// let booleanisLoggedIn= Boolean(isLoggedIn)
+// console.log(typeof booleanisLoggedIn)
+// console.log(booleanisLoggedIn)
 
 // 1 => true
 // 0 => false
 // "" => false
 // "harsh" => true
+  
+// Operations 
+
+let value=3
+let negVlaue= -value
+// console.log(negVlaue)
+
+let str1="harsh"
+let str2=" hello"
+let str3= str1+str2
+console.log(str3);
+
+// console.log("1" + 2 + 2);
+
+// console.log(+true)
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter)
