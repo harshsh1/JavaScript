3744a7b97426f18790432b0577a6a1f51461b320
Harsh <hs6682100@gmail.com>
2023-12-20 18:51:37 +0000

    Datatypes & Conversion

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all javscript code as newer version
+
+// alert("hello") // we are using nodejs,not browser
+// Code readability should be high
+
+let name= "harsh"
+let age= 21
+let isLoggedIn= true
+
+// number
+// bigint
+// string(single as well as double quote)
+// boolean => true/false
+// null =>standalone value
+// undefined =>
+// symbol (For finding uniqueness)
+
+
+// object
+
+console.log(typeof age);
+
+
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,23 @@
+// let score= undefined
+
+// console.log(typeof (score));
+
+// let valueInNumber= Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc"=> NaN
+// true => 1;
+// false => 0
+
+let isLoggedIn= "Harsh"
+
+let booleanisLoggedIn= Boolean(isLoggedIn)
+console.log(typeof booleanisLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "harsh" => true
