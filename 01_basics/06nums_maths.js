313d5afce55baf5f037ef5dbd72e01f3661f7bbb
Harsh <hs6682100@gmail.com>
2023-12-28 17:03:51 +0000

    Numbers

---
+++ 01_basics/06nums_maths.js
@@ -0,0 +1,19 @@
+const score= 400
+// console.log(score);
+const balance=new Number(100)
+
+// console.log(balance);
+// console.log(typeof balance);
+
+// console.log(typeof balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const other=123.8966
+
+// console.log(other.toPrecision(5));
+
+const hundreds= 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++  Maths  +++++++++++++++++++++++++++++
+
