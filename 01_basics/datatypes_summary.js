008138b9a469c957f626339784cf62055cd12fe7
Harsh <hs6682100@gmail.com>
2023-12-28 10:30:00 +0000

    strings

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -21,3 +21,27 @@ const myFunction=function(){
 }
 
 console.log(typeof myFunction);
+
+
+// Memory
+// Stack(Primitive),Heap(Non-Primitive)
+
+
+let myYoutubename="harshdotcom"
+
+let anothername=myYoutubename  // anothername only gets a copy of myyoutube name
+
+anothername="chai aur code";
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne={
+    email:"harsh@gmail.com",
+    upi:"user@paytm"
+}
+
+let userTwo=userOne;   //userTwo get the original refernce from heap
+userTwo.email="shivam@gmail.com"
+
+console.log(userOne);
+console.log(userTwo);
