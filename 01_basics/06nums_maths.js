f020dd53963e17ed187fe7543986a1cadcfddcb5
Harsh <hs6682100@gmail.com>
2023-12-29 17:53:43 +0000

    Number

--- 01_basics/06nums_maths.js
+++ 01_basics/06nums_maths.js
@@ -17,3 +17,18 @@ const hundreds= 1000000
 
 //+++++++++++++++++  Maths  +++++++++++++++++++++++++++++
 
+console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,3,5,6));
+
+// console.log(Math.random()); // Generate values in range of [0-1]
+// console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
