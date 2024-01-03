d062a0359293bf6cfe023b20f074a77bc1090e12
Harsh <hs6682100@gmail.com>
2024-01-03 17:02:19 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,27 @@
+const myArray = [1,2,3,4,5,true]
+const myHeroes = ["Kobe","david"];
+const myArr2=new Array(1,2,3,4)
+
+
+// Arrays Methods
+
+// myArray.push(6)
+// myArray.pop()
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(4));
+
+// const newArray= myArray.join()
+// console.log(myArray);
+// console.log(newArray);
+
+// slice,splice
+
+// console.log("A ",myArray);
+
+// const myn1 = myArray.slice(1,3)
+// console.log(myn1);
+// console.log("B ",myArray);
+
+const myn2 = myArray.splice(1,3)
+console.log(myn2);
+console.log("B ",myArray);
