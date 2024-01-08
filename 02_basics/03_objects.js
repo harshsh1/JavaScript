da0b450c9072b8784aeeaa695469eb7203a565d1
Harsh <hs6682100@gmail.com>
2024-01-08 13:23:30 +0000

    Objects part 1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,37 @@
+// singleton (through constructor apna tara ka ek hi object ha)
+
+// object literals
+//Object.create(using constructor)
+const mySym= Symbol("key1")
+const JsUser={               // Not a singleton
+    name:"Harsh",
+    "full name": "Harsh Sharma",
+     [mySym] : "mykey1",
+    age : 22,
+    location :"hapur",
+    email: "harsh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Sunday"]
+}
+
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email="harsh@gmail.com"  // Overwriting email
+// Object.freeze(JsUser)  // We have freeze  the object no overWriting can be done
+JsUser.email="harsh@amazon.com"  // Value will not change
+// console.log(JsUser.email);   
+// console.log(JsUser);
+
+JsUser.greeting = function()
+{
+  console.log("heloo Js user");
+}
+
+JsUser.greetingTwo = function()
+{
+  console.log(`hello Js user, ${this.name}`);
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
