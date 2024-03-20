fc0cc39628d76226d928cda1ef79c5aaea6aed47
Harsh <hs6682100@gmail.com>
2024-03-20 14:21:17 +0000

    IIFE functions

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,9 @@
+// Immediately Invoked Function Expressions(IIFE)
+
+(function chai (){  //named IIFE
+    console.log(`DB Connected`);
+})();
+
+( (name)=>{       // Unnamed IIFE
+    console.log(`DB Connected Two ${name}`);
+})("Harsh")
