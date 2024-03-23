0796c02d7df3e7d2b1f29f23f3fce2c97b2f875e
Harsh <hs6682100@gmail.com>
2024-03-23 19:38:10 +0000

    filter

---
+++ 05_iteration/02_filter.js
@@ -0,0 +1,13 @@
+const myNums = [1,2,3,4,5,6,7,8,10]
+
+//console.log(myNums.filter( (num) => num > 4))   //Filter can return a value
+/*
+const newNums = []
+
+myNums.forEach( (num)=>{
+    if(num>4){
+        newNums.push(num)
+    }
+})
+console.log(newNums);
+*/
